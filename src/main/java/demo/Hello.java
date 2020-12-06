ed3ae05a6a974d34edefd6e852e5d9f221745fb6
daranha1@yahoo.ca <daranha1@yahoo.ca>
2020-12-06 17:11:44 -0500

    Updated Hello file

--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -3,6 +3,7 @@ package demo;
 public class Hello {
 
     public static void  main (String[] args) {
-                  System.out.println("Hello World");
+            System.out.println("Hello World");
+            System.out.println("Jenkins Test");
     }
 }
